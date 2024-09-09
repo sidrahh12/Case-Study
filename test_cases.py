48c2b0716dd548613581516b42132c4725e01899
sidrahh12 <sidrah12a@gmail.com>
2024-09-09 16:55:28 +0530

    Update dependencies

--- test_cases.py
+++ test_cases.py
@@ -11,12 +11,13 @@ import re
 # Setting Tesseract path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-# Getting API key froos.getenv('GEMINI_API_KEY')m environment variable
+# Getting API key from environment variable
 api_key = 'AIzaSyCDDrJ6pqDjnU3zcJffyciYzhSCog9Efw4'
 
 if not api_key:
     raise ValueError("GEMINI_API_KEY environment variable is not set")
 genai.configure(api_key=api_key)
+
 
 st.title("Test Case Generator")
 
